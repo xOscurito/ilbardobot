eb47d01888ec814063546f5a9b5dc21a30b03238
Oscurito <waffufaccia@gmail.com>
2021-11-24 21:18:47 +0100

    prima prova

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
+const { channel } = require('diagnostics_channel');
 const Discord = require('discord.js');
 const client = new Discord.Client();
- 
-const prefix = 'ld!';
-const prefixx = 'LD!';
-const prefixxx = 'Ld!';
- 
 const fs = require('fs');
  
+const ytdl = require("ytdl-core")
+
+const prefix = 'm!';
+const prefixx = 'M!';
+ 
+var servers = {}; 
+
 client.commands = new Discord.Collection();
  
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
@@ -19,97 +22,68 @@ for(const file of commandFiles){
  
 client.once('ready', () => {
     console.log('Bot ora avviato');
-    client.user.setActivity("ld!help");
+    client.user.setActivity("m!p");
 });
  
 client.on('message', message =>{
-    if(!message.content.startsWith(prefix) && !message.content.startsWith(prefixx) && !message.content.startsWith(prefixxx) || message.author.bot) return;
- 
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
+
+
+    if(!message.content.startsWith(prefix) && !message.content.startsWith(prefixx) || message.author.bot) return;
+
+    let args = message.content.substring(prefix.length).split(" ");
+
+    switch(args[0]){
+        case 'play':
+            function play(connection, message){
+                var server = servers[message.guild.id];
+                server.dispatcher = connection.playStream(ytdl(server.queue[0], {filter: "audioonly"}));
+
+                server.queue.shift();
+
+                server.dispatcher.on("end", function(){
+                    if(server.queue[0]){
+                        play(connection, message);
+                    }else{
+                        connection.disconnect();
+                    }
+                })
+
+
+
+            }
+
+
+
+            if(!args[1]){
+                message.channel.send("Il mio panflute ha bisogno di un link... :C");
+                return;
+            }
+            if(!args[1].startsWith("https")){
+                message.channel.send("Il mio panflute accetta solo link.. :/");
+                return;
+            }
+            if(!message.member.voice.channel){
+                message.channel.send("Il mio panflute serve solo le persone nei canali vocali.. T_T");
+                return;
+            }
+            if(!servers[message.guild.id]) servers[message.guild.id]= {
+                queue: []
+            }
+
+            var server = servers[message.guild.id];
+
+            server.queue.push(args[1]);
+
+
+            if(message.guild.voice.connection) message.member.voice.channel.join().then(function(connection){
+                play(connection, message);
+            })
+
+
+        break;
+    }
     
-    if(command === 'help' || command === 'list' || command === 'info'){
-        client.commands.get('list').execute(message, args);
-    } 
-        //TECHS
-    else if(command === 'ada' || command === 'mindfullness' || command === 'all_day_awareness'){
-        client.commands.get('ada').execute(message, args);
-    } 
-    else if(command === 'cild'){
-        client.commands.get('cild').execute(message, args);
-    }
-    else if(command === 'deild'){
-        client.commands.get('deild').execute(message, args);
-    }
-    else if(command === 'fild' || command === 'finger' || command === 'fingers'){
-        client.commands.get('fild').execute(message, args);
-    }
-    else if(command === 'imp' || command === 'impossible_movement' || command === 'impossible' || command === 'movement' || command === 'impossible_movement_practice'){
-        client.commands.get('imp').execute(message, args);
-    }
-    else if(command === 'love_method' || command === 'love' || command === 'daniel_love' || command === 'the_love_method' || command === 'tlm'){
-        client.commands.get('lovemethod').execute(message, args);
-    }
-    else if(command === 'mild'){
-        client.commands.get('mild').execute(message, args);
-    }
-    else if(command === 'rausis'){
-        client.commands.get('rausis').execute(message, args);
-    }
-    else if(command === 'spild'){
-        client.commands.get('spild').execute(message, args);
-    }
-    else if(command === 'ssild' || command === 'senses' || command === 'sense'){
-        client.commands.get('ssild').execute(message, args);
-    }
-    else if(command === 'vild' || command === 'visual'){
-        client.commands.get('vild').execute(message, args);
-    }
-    else if(command === 'wbtb'){
-        client.commands.get('wbtb').execute(message, args);
-    } 
-    else if(command === 'wild'){
-        client.commands.get('wild').execute(message, args);
-    }
-    else if(command === 'wrild' || command === 'write' || command === 'written'){
-        client.commands.get('wrild').execute(message, args);
-    }
-
-        //OTHERS
-        
-    else if(command === 'meditation'){
-        client.commands.get('meditation').execute(message, args);
-    } 
-    else if(command === 'best_techniqe' || command === 'best' || command === 'best_tech' || command === 'best_techniques'){
-        client.commands.get('besttech').execute(message, args);
-    } 
-    else if(command === 'binaural_beats' || command === 'bbs' || command === 'bb' || command === 'beats' || command === 'binaural'){
-        client.commands.get('binaural').execute(message, args);
-    } 
-    else if(command === 'dream_journal' || command === 'dream_journaling' || command === 'journal'){
-        client.commands.get('dreamjournal').execute(message, args);
-    }
-    else if(command === 'dream_longer' || command === 'longer' || command === 'stabilize'){
-        client.commands.get('dreamlonger').execute(message, args);
-    }
-    else if(command === 'fall_asleep_fast' || command === 'fallasleepfast' || command === 'fast'){
-        client.commands.get('fallasleepfast').execute(message, args);
-    }
-    else if(command === 'reality_check' || command === 'rc' || command === 'rcs' || command === 'realitycheck' || command === 'realitychecks' || command === 'reality_checks' || command === 'check'){
-        client.commands.get('realitycheck').execute(message, args);
-    }
-    else if(command === 'sc' || command === 'sleepcycle' || command === 'sleep_cycle'){
-        client.commands.get('sleepcycle').execute(message, args);
-    } 
-    else if(command === 'sp' || command === 'sleepparalysis' || command === 'sleep_paralysis'){
-        client.commands.get('sleepp').execute(message, args);
-    } 
-
-
-    else
-    {
-        message.channel.send('Invalid command. Type ld!list for a list of commands.')
-    }
+    
 });
  
 
